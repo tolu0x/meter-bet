35a523c2a9d8ff558f9973ffb484efca6b54e3a5
Tolu <kalejaiyetoluwalase204@gmail.com>
2023-04-06 11:55:45 +0100

    mobile responsiveness

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+    screens: {
+      sm: { max: "650px" },
+      lg: "1024px",
+    },
+  },
+  plugins: [],
+};
